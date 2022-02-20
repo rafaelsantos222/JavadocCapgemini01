d28eeb0b34b660f634da30382006f23287d3c2d7
rafaelsantos222 <100045090+rafaelsantos222@users.noreply.github.com>
2022-02-20 13:30:02 -0300

    Add files via upload

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"questao01capgemini","c":"Questao01capgemini","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"questao01capgemini","c":"Questao01capgemini","l":"Questao01capgemini()","u":"%3Cinit%3E()"},{"p":"questao01capgemini","c":"Questao01capgemini","l":"retorna(int)"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"questao01capgemini","l":"Questao01capgemini"}];updateSearchResults();
